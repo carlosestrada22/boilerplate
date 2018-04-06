620700506f86085531434daff1bbb09ae4ac75ef
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2018-04-06 16:52:12 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const css = require('./app.scss')
+
+const test = str => str.reverse || str
+
+console.log("es2016")
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: 'app.bundle.js'
+    },
+    devServer: {
+        compress: true,
+        port: 9000,
+        stats: "errors-only",
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            minimize: true,
+                            sourceMap: true
+                        }
+                    }, 'sass-loader']
+                })
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"]
+
+            },
+            {
+                test: /\.pug$/,
+                use: [{
+                    loader: "html-loader",
+                    options: {
+                        minify: false,
+                        minimize: false
+                    }
+
+                }, "pug-html-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "test vTex",
+            template: "./src/index.pug",
+            minify: {
+                collapseWhitespace: false,
+            },
+            hash: true,
+            filename: "index.html"
+        }),
+        new ExtractTextPlugin('style.css')
+    ]
+}
